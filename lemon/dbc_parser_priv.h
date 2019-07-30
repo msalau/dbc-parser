c28cbbca05e8b20e9d4b4911912ba0e4d0fa8e60
Maksim Salau <maksim.salau@gmail.com>
2019-07-30 21:57:49 +0300

    Add a parser using the lemon parser-generator

---
+++ lemon/dbc_parser_priv.h
@@ -0,0 +1,30 @@
+#ifndef DBC_PARSER_PRIV_H__
+#define DBC_PARSER_PRIV_H__
+
+#include <gmodule.h>
+
+typedef struct {
+    unsigned first_line;
+    unsigned first_column;
+    unsigned last_line;
+    unsigned last_column;
+} dbc_scanner_lloc_t;
+
+typedef struct {
+    gchar *filename;
+    gchar *token;
+    gchar *parse_error;
+
+    dbc_scanner_lloc_t lloc;
+} dbc_state_t;
+
+#define DBC_DEBUG
+//#define DBC_TRACE
+
+#ifdef DBC_DEBUG
+#define dbc_printf(...) printf(__VA_ARGS__)
+#else
+#define dbc_printf(...) (void)0
+#endif
+
+#endif
